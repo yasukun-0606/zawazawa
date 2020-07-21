06ba4a1b5019d04c4afdb8f935c6287e2c6f9abc
miya653465 <52948482+miya653465@users.noreply.github.com>
2020-07-21 11:35:10 +0900

    Add files via upload

---
+++ Eiyou.php
@@ -0,0 +1,31 @@
+<?php
+
+    //データベース接続情報ファイル（ログイン用）※ログインフォルダから動かさないでください。
+    require_once __DIR__ .'/config.php';
+
+    //セッションの引継ぎ    
+    session_start();
+    
+    //ログイン情報の保持
+    
+    $datasql="select * table user";
+    $stmt2=$pdo->prepare($datasql);
+    $result = $stmt2->fetch(PDO::FETCH_ASSOC);
+
+    $name=$result['user_id'];
+    $id=$result['user_name'];
+
+    //入力内容の保持    
+    $age = $_POST['age'];  //年齢
+    $gender = $_POST['gender']; //性別  
+    $height = $_POST['height']; //身長
+    $weight = $_POST['weight']; //体重
+    $moment = $_POST['moment']; //運動強度  
+    $eight = $_POST['target']; //目標
+    
+    $sql = "insert into user_table(id,name,age,height,weight,moment,eight) values(?, ?, ?, ?, ?, ?, ?)";   //valuesで値を受け取る。カラムに値を格納できるようにする 
+    $stmt = $pdo->prepare($sql); //SQL文をセットしてデータベースに接続
+    $stmt->execute([$id, $name, $age, $height, $weight, $moment, $eight]); 
+    
+    //header('calendar.php');
+?>
