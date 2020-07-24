e4a807220e9137c27d9a1a9f98515e2d9d0135af
yasukun-0606 <68521444+yasukun-0606@users.noreply.github.com>
2020-07-24 10:14:07 +0900

    Add files via upload

--- nutorition/registnutrition.php
+++ nutorition/registnutrition.php
@@ -1,7 +1,16 @@
 <?php
-include 'config.php';
+//include 'config.php';
 //include 'function.php';
+require_once('../config.php');
+
+/* 
+セッション時に月と日とユーザーネームを取る 
+ユーザーネームからユーザーidを抽出
+postで結果表示画面に月日とユーザーidとユーザーネームを送ること-->
+///0724にやるべきこと　jsからcountとlogarrayを取って確定ボタンを押したらデータ挿入を行うものを作る
+*/
 ?>
+
 
 <!DOCTYPE html>
 
@@ -34,38 +43,141 @@ include 'config.php';
 // DB接続しSQLを発行してデータを取得
 $sql= "select * from materials";
 $stmt = $pdo -> prepare($sql);
-$stmt->execute();
-$result = $stmt->fetch(PDO::FETCH_ASSOC); 
+$stmt->execute(); 
 
-//プルダウン作成
-echo "<select class='MA_selectnamebox'>";
-echo "<select id='MA_name' name='MA_Name' class='MA_selectnamebox'>";
-while( $res = $stmt->fetch(PDO::FETCH_ASSOC) ){  // 実行結果から1レコード取ってくる
-  	$names = $res['materials'];
-  	$kcal = $res['calories'];
+//プルダウン作成+
+echo "<form method='post'>";
+echo "<select id='MA_name' name='MA_name' class='MA_selectnamebox'>";
+while($res = $stmt->fetch(PDO::FETCH_ASSOC)){  // 実行結果から1レコード取ってくる
+  	$names=$res['materials'];
+  	$kcal=$res['calories'];
   	echo "<option value='$names'>$names</option>";
 }	
   echo "</select>"; 
+  echo "<input type='text' id='MA_num' name='MA_Num'placeholder='個'class='MA_numbox'>";
+  echo "<button class='MA_registbutton'type='button' name='MA_add' value='ボタン' onclick='MA_clickgo()'>決定</button>";
+
+  echo "</form>";
 ?>
 
 <form method="post">
 <p><label for="name"class="MA_numlabel">個数</label>
 <label for="name"class="MA_attention">(※肉類は100gで1個として扱います)</label>
-<input type="text" id="MA_num" name="MA_Num"placeholder="個"class="MA_numbox">
 </p>
-<input type="button" class="MA_registbutton"  name="add" value="決定!" onclick="MA_clickgo()"><br>
+
+<!-- <input type="button" class="MA_registbutton"  name="MA_add" value="決定!" onclick="MA_clickgo()"><br> -->
 </form>
 
 
 
 
+ <!----------------------------------料理名のブロック----------------------------------->                                  
+</div>
+<div style="float:left;" class="inputformat">
+<br>
+<form method="post">
+<label for="name" class="ME_biglabel">  料理名  </label>   
+<p><label for="name" class="ME_selectnamelabel">料理名選択</label> </p>
+</p>
+<p><label for="name" class="ME_numlabel">個数</label>
+<input id="ME_num"type="text" name="ME_Num" class="ME_numbox"placeholder="個">
+</form>
+<!-- 料理名のプルダウン作成 -->
+<?php
+
+$sql= "select * from dishes";
+$stmt = $pdo -> prepare($sql);
+$stmt->execute();
+echo "<form method='post'>";
+echo "<select id='ME_name' name='ME_Name' class='ME_selectnamebox'>";
+while($res=$stmt->fetch(PDO::FETCH_ASSOC)){  // 実行結果から1レコード取ってくる
+	  $names=$res['dishes'];
+   	  $kcal=$res['calories'];
+	echo "<option value='$names'>$names</option>";
+}	
+   echo "</select>";
+   echo "</form>";
+?>
+
+</p>
+<!-- <input type="button" class="ME_registbutton"  name="add" value="決定!" onclick="ME_clickgo()"><br> -->
+<button class="ME_registbutton"type="submit" name="ME_add" value="決定！" onclick="ME_clickgo()">決定</button> 
+</form>
+
+
+ <!----------------------------------その他のブロック----------------------------------->                                  
+
+</div>
+<div style="float:left;"class="inputformat">
+
+<br>
+
+<label for="name" class="OT_biglabel">  その他  </label>   
+<p><label for="name" class="OT_selectnamelabel">品目名選択</label> </p>
+<label for="name" class="OT_label1">登録場所</label>
+<label for="name" class="OT_label2">登録名</label>
+<label for="name" class="OT_label3">カロリー</label>
+
+<?php
+$sql= "select * from foods";
+$stmt = $pdo -> prepare($sql);
+$stmt->execute();
+
+ // 取得した内容を表示する
+echo "<form method='post'>";
+echo "</p><p><label for='name' class='OT_numlabel'>個数</label>";
+echo "<input id='OT_dnum' type='text' name='OT_Num' class='OT_numbox' placeholder='個'>";
+echo "</p><button class='OT_registbutton' type='button' name='OT_add' value='決定！' onclick='OT_clickgo()' >決定</button>";
+
+echo "</form>";
+//  --------- その他のプルダウン作成 ----------
+echo "<select id='OT_dname' name='OT_Name' class='OT_selectnamebox'>";
+while( $res = $stmt->fetch(PDO::FETCH_ASSOC) ){  // 実行結果から1レコード取ってくる
+   $names    = $res['foods'];
+   $kcal    = $res['calories'];
+   echo "<option value='$names'>$names</option>";
+ }	
+   echo "</select>";
+?>
+
+<form method="post">
+</p>
+<p><label for="name" class="OT_numlabel">個数</label>
+<input id="OT_dnum" type="text" name="OT_Num" class="OT_numbox"placeholder="個">
+</p>
+
+<button class="OT_registbutton"type="button" name="OT_add" value="決定！" onclick="OT_clickgo()" >決定</button>
+<label for="name" class="OT_label1">登録場所</label>
+<label for="name" class="OT_label2">登録名</label>
+<label for="name" class="OT_label3">カロリー</label>
+</form>
+
+<form method="post">
+<p class = OT_ppos>
+<input type="radio" name="OT_radio" value="1" checked>食品名<br>
+<input type="radio" name="OT_radio" value="2" checked>料理名<br>
+<input type="radio" name="OT_radio" value="3" checked>その他<br>
+</p>
+<input id="OT_registname"type="text" name="OT_name" class="OT_box1"placeholder="名前">
+<input id="OT_registkcal"type="text" name="OT_kcal" class="OT_box2"placeholder="kcal">
+<button class="OT_dataregistbutton"type="submit" name="dataregist" value="ボタン" onclick="window.location.reload(true);" >新規登録</button>
+</form>
+
+
+</div>
+<div style="float:left;"class="displayformat">
+<!-- <div class="registbox">登録情報</div> -->
+<input type="text" id="registfield" name="regist" class="registbox" placeholder="登録情報">
+
+<input type="button" class="backbutton"  name="back" value="データを1つ取り消す" onclick="clickback()"><br>
+
 <?php
 //新規登録処理
 	if(isset($_POST['dataregist'])) {//新規登録のボタンが押された時
-		echo $checkradio=$_POST['OT_radio'];
-		echo $checkname=$_POST['OT_name'];
-		echo $checkkcal=$_POST['OT_kcal'];
-		$repeat=false;//重複チェック変数
+		 $checkradio=$_POST['OT_radio'];
+		 $checkname=$_POST['OT_name'];
+		 $checkkcal=$_POST['OT_kcal'];
+		 $repeat=false;//重複チェック変数
 		
 			if(!empty($checkname) && !empty($checkkcal)){
 			//空白データのチェック
@@ -105,7 +217,7 @@ while( $res = $stmt->fetch(PDO::FETCH_ASSOC) ){  // 実行結果から1レコー
 							$repeat=true;
 						break;
 						}
-						$sql= "INSERT INTO foods(calories,calories) VALUES(?,?)";
+						$sql= "INSERT INTO foods(foods,calories) VALUES(?,?)";
 						break;
 					default:
 						echo "ラジオボタンに変な値はいってる";
@@ -122,171 +234,62 @@ while( $res = $stmt->fetch(PDO::FETCH_ASSOC) ){  // 実行結果から1レコー
 			 	
 			
 		}
-		else echo "登録データを入力してください";
 	}
 
 
 //決定を押したときに、リザルトで表示するDBへのデータ挿入
-if(isset($_POST['add'])) {//決定が押された時！ うまくいかなければそれぞれの結果ボタンの名前をMAとかつけて論理和で結ぶ
-	if($_POST['add']=='MA_Name'){//食品名のとき
-		echo $checkradio=1;//id
-		echo $checkname=$_POST['MA_Name'];//名前
-		echo $checknum=$_POST['MA_Num'];//個数
-		$sql="select calories from materials where materials = :names ";
-		$stmt = $pdo -> prepare($sql);
-		$stmt->bindParam(':names', $dname, PDO::PARAM_STR,30);
-		$stmt->execute();
-		$hit = $stmt->fetch(PDO::FETCH_ASSOC);
-	
-		echo $resultkcal=$hit*$checknum;//総カロリー
-	}
-	if($_POST['add']=='ME_Name'){//料理名
-		echo $checkradio=2;//id
-		echo $checkname=$_POST['ME_Name'];//名前
-		echo $checknum=$_POST['ME_Num'];//個数
-		$sql="select calories from dishes where dishes = :names ";
-		$stmt = $pdo -> prepare($sql);
-		$stmt->bindParam(':names', $dname, PDO::PARAM_STR,30);
-		$stmt->execute();
-		$hit = $stmt->fetch(PDO::FETCH_ASSOC);
-		echo $resultkcal=$hit*$checknum;//総カロリー
-	}
-	if($_POST['add']=='OT_Name'){//料理名
-		echo $checkradio=3;//id
-		echo $checkname=$_POST['OT_Name'];//名前
-		echo $checknum=$_POST['OT_Num'];//個数
-		$sql="select calories from foods where foods = :names ";
-		$stmt = $pdo -> prepare($sql);
-		$stmt->bindParam(':names', $dname, PDO::PARAM_STR,30);
-		$stmt->execute();
-		$hit = $stmt->fetch(PDO::FETCH_ASSOC);
-		echo $resultkcal=$hit*$checknum;//総カロリー
-		}
+//if(!empty($_POST['MA_add'])||!empty($_POST['ME_add'])||!empty($_POST['OT_add'])) {//決定が押された時！ うまくいかなければそれぞれの結果ボタンの名前をMAとかつけて論理和で結ぶ
+if(!empty($_POST['MA_add'])){
 
-		if(!empty($checkname) && !empty($checknum)){
-		//空白データのチェック
-			$sql="INSERT INTO nutritionreg_table(UserID,date,DetaName,Calorie,WorkTime) VALUE('aaa',2020-07-01,?,?,?)";//　結合時ユーザーIDと日付データも挿入すること正しいカラム名に変更すること
-			$stmt = $pdo -> prepare($sql);
-			$stmt->execute($checkname,$checknum,$resultkcal);//本来もう少し増える
-			$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		}
+	echo "<script>alert('テスト');</script>"; 
+	if(!empty($_POST['MA_add'])&&!empty($_POST['MA_name'])){//食品名のとき
+	 	$checkradio=1;//id
+		$checkname=$_POST['MA_name'];//名前
+		$checknum=$_POST['MA_Num'];//個数
+		$sql="select calories from materials where materials = :names ";
+	 }
+	 else if(isset($_POST['ME_add'])&&!empty($_POST['ME_name'])){//料理名のとき
+		echo $checkradio=2;//id
+	    echo $checkname=$_POST['ME_Name'];//名前
+	    echo $checknum=$_POST['ME_Num'];//個数
+	    $sql="select calories from dishes where dishes = :names ";
+	 }
+	else if(isset($_POST['OT_add'])&&!empty($_POST['OT_Name'])){//その他のとき
+		echo $checkradio=3;//id
+	    echo $checkname=$_POST['OT_Name'];//名前
+	    echo $checknum=$_POST['OT_Num'];//個数
+	    $sql="select calories from foods where foods = :names ";
+	 }
+
+	 if(!empty($checkname)&&!empty($checknum)){
+		$stmt = $pdo -> prepare($sql);
+		$stmt->bindParam(':names', $dname, PDO::PARAM_STR,30);
+		$stmt->execute();
+		$hit = $stmt->fetch(PDO::FETCH_ASSOC);
+		$resultkcal=$hit*$checknum;//総カロリー
+		$sql="INSERT INTO nutritionreg_table(UserID,Date,DetaName,Calorie,WorkTime) VALUE('aaa',2020-07-01,?,?,?)";//　結合時ユーザーIDと日付データも挿入すること正しいカラム名に変更すること
+		$stmt = $pdo -> prepare($sql);
+		$stmt->execute($checkname,$checknum,$resultkcal);//本来もう少し増える
+		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	 }
+}
+
+?>
+
+<?php 
+//ひとつもどるを押したときに、リザルトで表示するDBへのデータ挿入
+if(isset($_POST['back'])) {//modoruが押された時！
+	if(empty($_POST['back'])){	 
 	}
 	else{
-		echo "登録データを入力してください";
-	}
-  ?>
-
-
-
-
-
-
-
- <!----------------------------------料理名のブロック----------------------------------->                                  
-</div>
-<div style="float:left;" class="inputformat">
-<br>
-<form method="post">
-<label for="name" class="ME_biglabel">  料理名  </label>   
-<p><label for="name" class="ME_selectnamelabel">料理名選択</label> </p>
-</p>
-<p><label for="name" class="ME_numlabel">個数</label>
-<input id="ME_num"type="text" name="ME_Num" class="ME_numbox"placeholder="個">
-</form>
-<!-- 料理名のプルダウン作成 -->
-<?php
-$sql= "select * from dishes";
-$stmt = $pdo -> prepare($sql);
-$stmt->execute();
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-echo "<select id='ME_name' name='ME_Name' class='ME_selectnamebox'>";
-while($res=$stmt->fetch(PDO::FETCH_ASSOC)){  // 実行結果から1レコード取ってくる
-	  $names=$res['names'];
-   	  $kcal=$res['kcal'];
-	echo "<option value='$names'>$names</option>";
-}	
-   echo "</select>";
-?>
-
-</p>
-<!-- <input type="button" class="ME_registbutton"  name="add" value="決定!" onclick="ME_clickgo()"><br> -->
-<button class="ME_registbutton"type="submit" name="add" value="ボタン" onclick="clickgo()">決定</button> -->
-</form>
-
-
- <!----------------------------------その他のブロック----------------------------------->                                  
-
-</div>
-<div style="float:left;"class="inputformat">
-
-<br>
-
-<label for="name" class="OT_biglabel">  その他  </label>   
-<p><label for="name" class="OT_selectnamelabel">品目名選択</label> </p>
-
-<!-- その他のプルダウン作成 -->
-<?php
-$sql= "select * from foods";
-$stmt = $pdo -> prepare($sql);
-$stmt->execute();
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-
- // 取得した内容を表示する
-echo "<select id='OT_name' name='OT_Name' class='OT_selectnamebox'>";
-while( $res = $stmt->fetch(PDO::FETCH_ASSOC) ){  // 実行結果から1レコード取ってくる
-   $names    = $res['foods'];
-   $kcal    = $res['calorie'];
-   echo "<option value='$names'>$names</option>";
- }	
-   echo "</select>";
-?>
-
-<form method="post">
-</p>
-<p><label for="name" class="OT_numlabel">個数</label>
-<input id="OT_num" type="text" name="OT_Num" class="OT_numbox"placeholder="個">
-</p>
-<button class="OT_registbutton"type="submit" name="add" value="決定！" onclick="OT_clickgo()" >決定！</button>
-<label for="name" class="OT_label1">登録場所</label>
-<label for="name" class="OT_label2">登録名</label>
-<label for="name" class="OT_label3">カロリー</label>
-</form>
-
-
-<form method="post">
-<p class = OT_ppos>
-<input type="radio" name="OT_radio" value="1" checked>食品名<br>
-<input type="radio" name="OT_radio" value="2" checked>料理名<br>
-<input type="radio" name="OT_radio" value="3" checked>その他<br>
-</p>
-<input id="OT_registname"type="text" name="OT_name" class="OT_box1"placeholder="名前">
-<input id="OT_registkcal"type="text" name="OT_kcal" class="OT_box2"placeholder="kcal">
-<button class="OT_dataregistbutton"type="submit" name="dataregist" value="ボタン" onclick="window.location.reload(true);" >新規登録</button>
-</form>
-
-
-</div>
-<div style="float:left;"class="displayformat">
-<!-- <div class="registbox">登録情報</div> -->
-<input type="text" id="registfield" name="regist" class="registbox" placeholder="登録情報">
-<input type="button" class="backbutton"  name="back" value="データを1つ取り消す" onclick="clickback()"><br>
-<?php 
-//決定を押したときに、リザルトで表示するDBへのデータ挿入
-if(isset($_POST['back'])) {//modoruが押された時！
-		$day=20200701;//本来はユーザーIDと日付をもらう
+		$day=2020-07-01;//本来はユーザーIDと日付をもらう
 		$sql="DELETE FROM nutritionreg_table WHERE Date = ? ORDER BY DATE AS DESC LIMIT 1";//　結合時ユーザーIDと日付データも挿入すること正しいカラム名に変更すること
 		$stmt = $pdo -> prepare($sql);
 		$stmt->execute($day);//本来もう少し増える
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
+	}
 }
-else {
-	echo "登録データを入力してください";
-}
-
   ?>
-
-
-
 
 <form action="registexercise.php" method="post">
 <input type="submit" class="jumpexercise"  name="button" value="運動データ登録へ" ><br>
