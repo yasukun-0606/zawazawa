799ffa71a96cf7ee47cc8cf1207ead85dfe87823
sakaikaduki <68530498+sakaikaduki@users.noreply.github.com>
2020-07-27 17:26:06 +0900

    エラー処理の表示を治しました

--- body_temp/check.php
+++ body_temp/check.php
@@ -12,20 +12,22 @@
     <title>体調管理</title>
 </head>
 <header>
-  <h1 class="title" align="center">確認画面</h1>
+  <link rel="stylesheet" href="check.css">
+  <h1 class="title">確認画面</h1>
 </header>
 <body bgcolor="skyblue">
 <div class="container">
 <span class="target" >
 <?php
-
+    session_start();
+    $name = $_SESSION['user_name'];
     $date = '';
 
-    //データベース接続情報ファイル（ログイン用）※ログインフォルダから動かさないでください。
-    require_once __DIR__ .'/config.php';
-
+    $input_day = '';                                              //日の初期化
     $input_month = $_POST['month'];                               //月の値を受け取る
-    $input_day = $_POST['day'];                                   //日の値を受け取る
+    if(!empty($_POST['day'])){
+      $input_day = $_POST['day'];                                   //日の値を受け取る
+    }
     $input_date = '2020-' . $input_month . '-' . $input_day;      //受け取った月日を合わせる
     $date = date('Y-m-d',strtotime($input_date));                 //date型に変換
     $temp = $_POST['temp_n'];                                     //体温を受け取る
@@ -34,12 +36,14 @@
     if($date == '' || $temp == '') 
     {
         echo '<form method="post" action="register.php">';
-        echo '<p> 未入力箇所があります</p>';
-        echo '<input type="button" value="戻る" onClick="history.back()">';
+        echo '<p><font size="5"> 未入力箇所があります</font></p>';
+        echo '<br>';
+        echo '<input style="width:100px " type="button" value="戻る" onClick="history.back()">';
     } else 
     //未入力箇所がない場合
     {
         echo '<form method="post" action="submit.php">';          //登録画面へのフォーム
+        echo '<font size="10">'. $name . 'さん</p>';
         echo '<p> <font size="10"> こちらの内容でよろしいですか？ <br /> <br /> </font> </p>';
         echo '<div style="display:inline-block; padding: 10px; margin-bottom: 10px; border: 1px solid #333333;">';
         echo '<input type="hidden" name="date_n" value="' . $date . '">';
