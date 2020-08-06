8aa2ac41a6e29ce3f5c748b53e7954fafc237918
yasukun-0606 <68521444+yasukun-0606@users.noreply.github.com>
2020-08-06 15:46:51 +0900

    完成品

--- nutorition/Eiyou.php
+++ nutorition/Eiyou.php
@@ -1,16 +1,6 @@
-<!--
-********************************************
-*This is update code
-*Name: Kazuya Miyagawa
-*CreateDate: 2020/07/10 11:40:00
-*Version: 1.00
-*Update: 2020/07/20
-*What to do:　入力したデータでユーザーのtableを作成する
-*******************************************
--->
 
 <?php
-    //データベース接続情報ファイル
+    //データベース接続情報ファイル（ログイン用）※ログインフォルダから動かさないでください。
     require_once('../config.php');
 
     //セッションの引継ぎ    
@@ -18,44 +8,46 @@
     $name =$_SESSION['user_name'];
     //ログイン情報の保持
     $chosesql="select * from user where user_name = ?";
+    //$databasesql="select * table user_name";
     $stmt=$pdo->prepare($chosesql);
     $stmt->execute([$name]);
-    //
+    //$result = $stmt->fetch(PDO::FETCH_ASSOC);
     foreach($stmt as $row){
         $username =$row['user_name'];
         $userid=$row['user_id'];
         $_SESSION['user_id']=$userid;
     }
+    //echo $name;
+    //echo $username;
+    //echo $userid;
 
-    //入力内容の保持 
+    //入力内容の保持    
+    $age = $_POST['age'];  //年齢
+    $gender = $_POST['gender']; //性別  
     $height = $_POST['height']; //身長
     $weight = $_POST['weight']; //体重
-    $age = $_POST['age'];  //年齢
-    $moment = $_POST['moment']; //運動強度 
-    $gender = $_POST['gender']; //性別  
+    $moment = $_POST['moment']; //運動強度  
     $eight = $_POST['target']; //目標
 
     //男女別の代謝計算用の値の判別
-    if($gender==1){
-        //男
+    if($gender==1){ //男
         $heightvalue=5;
         $weightvalue=13.8;
         $agevalue=6.8;
-    }else{ 
-        //女
+    }else{ //女
         $heightvalue=1.7;
         $weightvalue=9.6;
         $agevalue=7;
     }
-
     //代謝計算
     $metabo=66.5+($weight*$weightvalue)+($height*$heightvalue)+($age*$agevalue);
     
+    
+    echo $metabo;
     //データの登録　id、名前、身長、体重、年齢、運動強度、性別、代謝、目標
     $sql = "insert into user_table(id,name,height,weight,age, movement, gender, metabolism, target) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";   //valuesで値を受け取る。カラムに値を格納できるようにする 
     $stmt = $pdo->prepare($sql); //SQL文をセットしてデータベースに接続
     $stmt->execute([$userid,$username,$height,$weight,$age,$moment,$gender,$metabo,$eight]); 
-
     //カレンダー画面への遷移
     header('Location:calendar.php');
 ?>
---
+++ nutorition/Login.php
@@ -0,0 +1,121 @@
+<!--
+********************************************
+*This is Login display
+*Name: Katsuaki Matsushima
+*CreateDate: 2020/07/09 9:10:00
+*Version: 1.0
+*Update: 2020/07/20
+*******************************************
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>新規登録画面</title>
+    <link rel="stylesheet" href = "Login.css">　<!--CSS読み込み-->
+
+    <script type="text/javascript">  //Js読み込み
+
+    function check(){
+        var flag = 0;
+        // 設定開始（必須にする項目を設定してください）
+
+        if(document.user.age.value == ""){ // 「年齢」の入力をチェック
+            flag = 1;
+        }
+        else if(document.user.gender.value == ""){ // 「性別」の入力をチェック
+            flag = 1;
+        }
+        else if(document.user.height.value == ""){  // 「身長」の入力チェック
+            flag = 1;
+        }
+        else if(document.user.weight.value == ""){  // 「体重」の入力チェック
+            flag = 1;
+        }
+        else if(document.user.moment.value == ""){  // 「運動強度」の入力チェック
+            flag = 1;
+        }
+
+        // 現実的な数値のチェック
+        if(document.user.age.value > 130 || document.user.age.value < 0){          //年齢の数値チェック
+            flag = 2;
+        }
+        else if(document.user.height.value > 272 || document.user.height.value < 50){   //身長の数値チェック
+            flag = 2;
+        }
+        else if(document.user.weight.value > 150 || document.user.weight.value < 0){　//体重の数値チェック
+            flag = 2;
+        }
+
+        // 設定終了
+        if(flag == 1){
+            window.alert('必須項目に未入力がありました'); // 入力漏れがあれば警告ダイアログを表示
+            return false; // 送信を中止
+        }
+        else if (flag == 2){
+            window.alert('入力項目に誤りがあります'); // ありえない数値があれば警告ダイアログを表示
+            return false; // 送信を中止
+        }else{
+            return true; // 送信を実行
+        }
+    }
+    </script>
+
+</head>
+
+<header>
+      <h1 class="head">ユーザー情報登録</h1>        <!--ヘッダー情報-->
+</header>
+
+<body background ="back.png">               <!--背景設定-->
+    <br>
+    <form method="POST" action="Eiyou.php" name="user" onsubmit="return check()">        <!--ユーザー情報入力-->
+    <table border="0">　<!--表のふちを消す-->  
+        <tr>
+            <tb><b class="left">年齢</b></tb>                           <!--年齢入力-->
+            <tb>&emsp;<input class="input_form" type = "number" name ="age">歳</tb>　<!--年齢入力フォーム-->
+        </tr>
+        <tr>
+            <tb><b>&emsp;&emsp;&emsp;性別</b></tb>                          <!--性別入力-->
+            <tb>&emsp;<input type="radio" name="gender" value="1">男        <!--ラジオボタンで男と女を決める-->
+            &emsp;<input type="radio" name="gender" value="2">女</tb>
+        </tr>
+        </table>
+        <br>
+        <table border="0">              <!--表のふちを消す-->
+        <tr>
+            <tb><b class="left">身長(㎝)</b></tb>                           <!--身長入力-->
+            <tb><input class="input_form" type = "number" name ="height">㎝</tb>        <!--身長入力フォーム-->
+        </tr>
+        <tr>
+            <tb><b class="left">&emsp;体重(kg)</b></tb>                     <!--体重入力-->
+            <tb><input class="input_form" type = "text" name ="weight">kg</tb>      <!--体重入力フォーム-->
+        </tr>
+    </table> 
+    <br>
+    <br>
+    <p class="text">普段の運動強度目安</p>        <!--運動強度の入力-->
+    <div class="Login_t1">日常生活の運動強度についての説明<br>          <!--運動強度の説明とラジオボタン-->
+    &emsp;<input type="radio" name="moment" value="1">1.生活の大部分が座っている(例：デスクワークが多い人)<br>　　<!--ラジオボタンで入力する-->
+    <input type="radio" name="moment" value="2">2.座位中心だが移動することが多い。あるいは軽いスポーツ等を<br>
+    &emsp;&emsp;している人(例：営業等で移動が多い人)<br>
+    &emsp;<input type="radio" name="moment" value="3">3.移動や肉体労働が多い人(例：工事現場で作業している人)<br>
+    上記で近いものを選択してください。</div>
+ 
+    <br>
+    <p class="text">目標(目標の体重や体系を入力)</p>           <!--目標入力-->
+        <textarea  class="text" type = "text" name ="target" cols="40"></textarea>      <!--入力フォームてテキストフォーム-->
+        <br>
+    <p><input class="send" type="submit" value="送信"></p> <!--カレンダー画面へ遷移-->
+    
+    </form>
+
+    <br>
+        <br>
+        <div>
+            <button class="margin1" type="submit" onclick="location.href='https://www.dpt-inc.co.jp/'" >ホームへ<!--ホーム画面へ遷移-->
+        </div>
+
+</body>
+</html>
